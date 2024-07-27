b567d6ff7d8ac375993dd555bd64c166d67102ae
v.karakai <v.karakai@pin-up.tech>
2024-07-27 17:45:50 +0300

    [HW-6] - implemented AddressBook

---
+++ address_book/name.py
@@ -0,0 +1,5 @@
+from address_book.field import Field
+
+
+class Name(Field):
+    pass
---
+++ address_book/record.py
@@ -0,0 +1,26 @@
+from address_book.name import Name
+from address_book.phone import Phone
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                break
+
+    def find_phone(self, phone):
+        return any(p.value == phone for p in self.phones)
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
